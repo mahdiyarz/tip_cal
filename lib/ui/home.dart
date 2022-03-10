3ea1bd9b5f4a44c2701826b12dff71ec7d564eda
mahdiyarz <m.arbabzi@gmail.com>
2022-03-11 01:01:42 +0330

    Format the project

---
+++ lib/ui/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TipCounter extends StatefulWidget {
+  TipCounter({Key? key}) : super(key: key);
+
+  @override
+  State<TipCounter> createState() => _TipCounterState();
+}
+
+class _TipCounterState extends State<TipCounter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
