4cb7bcaeb082b618a34a0fdadff0c2bfd61f9099
mahdiyarz <m.arbabzi@gmail.com>
2022-03-12 00:33:33 +0330

    Create our first box

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -11,7 +11,29 @@ class _TipCounterState extends State<TipCounter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            Container(
+              width: 350,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.amberAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('data'),
+                  Text('\$235'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
